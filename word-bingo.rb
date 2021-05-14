93c8f721d949c6cdf85b4f63fcd66125c95ad372
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-05-14 09:39:18 +0900

    Add input process

--- word-bingo.rb
+++ word-bingo.rb
@@ -1,9 +1,20 @@
 # ビンゴカードのサイズSを入力(3 <= S <= 1000)
+size = gets.to_i
 
 # ビンゴカードの単語Ai,jをS行*S列入力(1 <= Ai,jの文字数 <= 100)
+card_array = []
+size.times do
+  card_array << gets.chomp
+end
 
 # 選ばれる単語の個数Nを入力(1 <= N <= 2000)
+number = gets.to_i
 
 # 単語wiをN個入力(1 <= wiの文字数 <= 100)
+number.times do
+  chosen_array << gets.chomp
+end
+
+# カードの単語と選ばれた単語との判定
 
 # 結果をyes, noで出力
