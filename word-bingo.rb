3fcab508f8c21f9ba29092e4487419f544a5f8d0
Malakir3 <on.95.kimura.yasuyuki@gmail.com>
2021-05-15 16:57:10 +0900

    replace "array" to "ary"

--- word-bingo.rb
+++ word-bingo.rb
@@ -2,21 +2,21 @@
 size_num = gets.to_i
 
 # ビンゴカードの単語Ai,jをS行*S列入力(1 <= Ai,jの文字数 <= 100)
-card_array = []
+card_ary = []
 size_num.times do
   input = gets.chomp
-  # 入力された単語をスペース毎に区切ってrow_arrayに格納し、さらにcard_arrayに格納
-  row_array = input.split(" ")
-  card_array << row_array
+  # 入力された単語をスペース毎に区切ってrow_aryに格納し、さらにcard_aryに格納
+  row_ary = input.split(" ")
+  card_ary << row_ary
 end
 
 # 選ばれる単語の個数Nを入力(1 <= N <= 2000)
-number = gets.to_i
+try_num = gets.to_i
 
 # 単語wiをN個入力(1 <= wiの文字数 <= 100)
-chosen_array = []
-number.times do
-  chosen_array << gets.chomp
+chosen_ary = []
+try_num.times do
+  chosen_ary << gets.chomp
 end
 
 # カードの単語と選ばれた単語との判定
